b855d46a1207437e79b182f28ebac1b9cb70c311
DEDSEC-WEAPON\minh <mnguyenngoc69@gmail.com>
2024-11-18 16:48:17 +0700

    lay ra chu de bai hat (rỗng)

--- index.ts
+++ index.ts
@@ -5,13 +5,21 @@ dotenv.config();
 import { connect } from "./config/database";
 connect();
 
+import { Topic } from "./models/topic.model"
+
 const app: Express = express();
 const port: number = 3000;
 
 app.set('views', `${__dirname}/views`) //tìm đến thư mục tên là views
 app.set('view engine', 'pug') // xác định view engine có đuôi .pug
 
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false,
+  });
+  
+  console.log(topics);
+
   res.render("client/pages/topics/index");
 });
 
